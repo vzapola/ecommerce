faea492490e54735160dcb13af547652b3e02936
Vinicius Zapola <vzapola@hotmail.com>
2023-02-02 15:49:09 -0300

    git commit -m "adicionando notação de pedidos"

---
+++ tela_de_pedidos.py
@@ -0,0 +1,11 @@
+# aqui serão anotados os pedidos
+
+pedido = input('Registre aqui o seu pedido. Registre quantos produtos quiser e, para finalizar o pedido, basta apertar enter com a caixa vazia:')
+pedidos = []
+
+while pedido != '':
+    pedidos.append(pedido)
+    pedido = input('Registre aqui o seu pedido. Registre quantos produtos quiser e, para finalizar o pedido, basta apertar enter com a caixa vazia:')
+        
+print(f'Resumo do pedido: Os itens foram {pedidos}')
+    
