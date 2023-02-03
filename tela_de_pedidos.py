e9dbd5b666c4d0024b56bae26bd46cc81efa5b21
Vinicius Zapola <vzapola@hotmail.com>
2023-02-03 14:55:05 -0300

    m- "cardápio adicionado"

--- tela_de_pedidos.py
+++ tela_de_pedidos.py
@@ -1,3 +1,13 @@
+# cardápio:
+
+print('''Especificação   Código  Preço
+Hamburguer Vegano   100     R$ 1,20
+Porção de Fritas    101     R$ 1,30
+Porção de Mandioca  102     R$ 1,50
+Suco Diversos       103     R$ 1,20
+Torta de Chocolate  104     R$ 1,30
+Bolo de Laranja     105     R$ 1,00''')
+
 # aqui serão anotados os produtos
 
 vendas = []
