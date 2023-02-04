a4ed4f79c59a4bab6368e8b7a24594b206226517
Vinicius Zapola <vzapola@hotmail.com>
2023-02-04 16:52:21 -0300

    Incluindo o preço dos produtos

--- tela_de_pedidos.py
+++ tela_de_pedidos.py
@@ -1,12 +1,21 @@
+# Preço dos produtos
+
+M100 = 1.20
+M101 = 1.30
+M102 = 1.50
+M103 = 1.20
+M104 = 1.30
+M105 = 1.00
+
 # cardápio:
 
-print('''Especificação   Código  Preço
-Hamburguer Vegano   100     R$ 1,20
-Porção de Fritas    101     R$ 1,30
-Porção de Mandioca  102     R$ 1,50
-Suco Diversos       103     R$ 1,20
-Torta de Chocolate  104     R$ 1,30
-Bolo de Laranja     105     R$ 1,00''')
+print(f'''Especificação   Código  Preço
+Hamburguer Vegano   100     R$ {M100:.2f}
+Porção de Fritas    101     R$ {M101:.2f}
+Porção de Mandioca  102     R$ {M102:.2f}
+Suco Diversos       103     R$ {M103:.2f}
+Torta de Chocolate  104     R$ {M104:.2f}
+Bolo de Laranja     105     R$ {M105:.2f}''')
 
 # aqui serão anotados os produtos
 
